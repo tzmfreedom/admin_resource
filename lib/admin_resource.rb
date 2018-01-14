a9b9266f075b4a7dbf730626526838e391dcf572
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2018-01-14 18:12:59 +0900

    Update

--- lib/admin_resource.rb
+++ lib/admin_resource.rb
@@ -1,33 +1,44 @@
 # frozen_string_literal: true
 
-#
-# class Admin::HogeController
-#   include AdminResource
-#   actions :index
-#   permit_params :name
-# end
-
 require 'active_support/concern'
 require 'admin_resource/railtie'
-require 'admin_resource/helper'
 require 'admin_resource/version'
 require 'ransack'
 require 'kaminari'
 
 module AdminResource
-  include ::AdminResource::Helper
-  extend ::ActiveSupport::Concern
+  cattr_accessor :managed_models
 
-  # before_action :authenticate
-  included do
-    before_action :set_record, only: %i[show edit update]
-    before_action :set_link_variables
+  def self.included(base)
+    base.extend ClassMethods
+    base.class_eval do
+      before_action :set_record, only: %i[show edit update]
+      before_action :set_link_variables
 
-    helper_method :model_klass
+      helper_method :model_klass
+    end
+
+    self.managed_models ||= []
+    model_klass = base.controller_name.classify.constantize
+    self.managed_models << model_klass unless self.managed_models.include?(model_klass)
   end
 
+  def model_klass
+    @model_klass ||= controller_name.classify.constantize
+  end
+
+  DSL_METHODS = %W[permitted_params show_params index_params search_params eager_load]
+
+  DSL_METHODS.each do |method|
+    define_method(method) do
+      self.class.send(method)
+    end
+  end
+
+  private
+
   def record_params
-    params.require(:"#{model_klass.name.underscore}").permit(*permitted_fields)
+    params.require(:"#{model_klass.name.underscore}").permit(*permitted_params)
   end
 
   def set_record
@@ -36,15 +47,36 @@ module AdminResource
     @record = relation.find(params[:id])
   end
 
-  DSL_METHODS = %W[permitted_fields show_fields list_fields search_fields eager_load]
-
-  DSL_METHODS.each do |method|
-    define_method(method) do
-      self.class.send(method)
-    end
+  def set_link_variables
+    admin_show_path
+    admin_index_path
+    admin_edit_path
+    admin_new_path
   end
 
-  class_methods do
+  def admin_show_path
+    return if @record.nil?
+    helper_method_name = "admin_#{model_klass.name.underscore}_path"
+    @admin_show_path = respond_to?(helper_method_name) ? send(helper_method_name, @record.id) : nil
+  end
+
+  def admin_index_path
+    helper_method_name = "admin_#{model_klass.name.pluralize.underscore}_path"
+    @admin_index_path = respond_to?(helper_method_name) ? send(helper_method_name) : nil
+  end
+
+  def admin_edit_path
+    return if @record.nil?
+    helper_method_name = "edit_admin_#{model_klass.name.underscore}_path"
+    @admin_edit_path = respond_to?(helper_method_name) ? send(helper_method_name, @record.id) : nil
+  end
+
+  def admin_new_path
+    helper_method_name = "new_admin_#{model_klass.name.underscore}_path"
+    @admin_new_path = respond_to?(helper_method_name) ? send(helper_method_name) : nil
+  end
+
+  module ClassMethods
     ACTIONS = %W[new create index show edit update destroy]
     PER_PAGE = 30
 
@@ -87,13 +119,13 @@ module AdminResource
         relation = relation.eager_load(eager_load) if eager_load
         @q = relation.search(params[:q])
         @records = @q.result(distinct: true).page(params[:page]).per(PER_PAGE)
-        render 'admin/shared/index' if list_fields
+        render 'admin/shared/index' if index_params
       end
     end
 
     def define_show
       define_method(:show) do
-        render 'admin/shared/show' if show_fields
+        render 'admin/shared/show' if show_params
       end
     end
 
